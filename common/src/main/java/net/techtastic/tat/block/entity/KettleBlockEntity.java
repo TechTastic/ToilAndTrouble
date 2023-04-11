d536e914faff0e7e9ee459b00cfb16a2d583cf70
TechTastic <thetechnomancer123@gmail.com>
2023-04-11 15:04:05 -0500

    More Kettle Work

--- common/src/main/java/net/techtastic/tat/block/entity/KettleBlockEntity.java
+++ common/src/main/java/net/techtastic/tat/block/entity/KettleBlockEntity.java
@@ -1,16 +1,139 @@
 package net.techtastic.tat.block.entity;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.NonNullList;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.ContainerHelper;
+import net.minecraft.world.WorldlyContainer;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.player.StackedContents;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.StackedContentsCompatible;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.techtastic.tat.block.TATBlockEntities;
+import org.jetbrains.annotations.Nullable;
 
-public class KettleBlockEntity extends BlockEntity {
+public class KettleBlockEntity extends BaseContainerBlockEntity implements StackedContentsCompatible, WorldlyContainer {
+    public NonNullList<ItemStack> inventory;
+    private int craftProgress = 0;
+    private final int maxCraftProgress = 0;
+
+
     public KettleBlockEntity(BlockPos blockPos, BlockState blockState) {
         super(TATBlockEntities.KETTLE_BLOCK_ENTITY.get(), blockPos, blockState);
     }
 
     public static void tick(Level level, BlockPos pos, BlockState blockState, BlockEntity blockEntity) {
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag compoundTag) {
+        ContainerHelper.saveAllItems(compoundTag, this.inventory);
+        compoundTag.putInt("ToilAndTrouble$craftProgress", this.craftProgress);
+
+        super.saveAdditional(compoundTag);
+    }
+
+    @Override
+    protected Component getDefaultName() {
+        return new TranslatableComponent("block.tat.kettle");
+    }
+
+    @Override
+    protected AbstractContainerMenu createMenu(int i, Inventory inventory) {
+        return null;
+    }
+
+    @Override
+    public void load(CompoundTag compoundTag) {
+        super.load(compoundTag);
+
+        this.inventory = NonNullList.withSize(this.getContainerSize(), ItemStack.EMPTY);
+        ContainerHelper.loadAllItems(compoundTag, this.inventory);
+
+        this.craftProgress = compoundTag.getInt("ToilAndTrouble$craftProgress");
+    }
+
+    @Override
+    public int[] getSlotsForFace(Direction direction) {
+        return new int[0];
+    }
+
+    @Override
+    public boolean canPlaceItemThroughFace(int i, ItemStack itemStack, @Nullable Direction direction) {
+        return direction != null && direction.equals(Direction.UP);
+    }
+
+    @Override
+    public boolean canTakeItemThroughFace(int i, ItemStack itemStack, Direction direction) {
+        return false;
+    }
+
+    @Override
+    public int getContainerSize() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        for (ItemStack stack : this.inventory) {
+            if (!stack.isEmpty())
+                return false;
+        }
+        return true;
+    }
+
+    @Override
+    public ItemStack getItem(int i) {
+        return this.inventory.get(i);
+    }
+
+    @Override
+    public ItemStack removeItem(int i, int j) {
+        ItemStack itemStack = ContainerHelper.removeItem(this.inventory, i, j);
+        if (!itemStack.isEmpty())
+            this.setChanged();
+        return itemStack;
+    }
+
+    @Override
+    public ItemStack removeItemNoUpdate(int i) {
+        ItemStack itemStack = this.inventory.get(i);
+        if (itemStack.isEmpty())
+            return ItemStack.EMPTY;
+        this.inventory.set(i, ItemStack.EMPTY);
+        return itemStack;
+    }
+
+    @Override
+    public void setItem(int i, ItemStack itemStack) {
+        this.inventory.set(i, itemStack);
+        if (!itemStack.isEmpty() && itemStack.getCount() > this.getMaxStackSize())
+            itemStack.setCount(this.getMaxStackSize());
+        this.setChanged();
+    }
+
+    @Override
+    public boolean stillValid(Player player) {
+        return true;
+    }
+
+    @Override
+    public void clearContent() {
+        this.inventory.clear();
+        this.setChanged();
+    }
+
+    @Override
+    public void fillStackedContents(StackedContents stackedContents) {
+        this.inventory.forEach(stackedContents::accountStack);
     }
 }
