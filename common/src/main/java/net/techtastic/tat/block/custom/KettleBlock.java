d28d9538890f638c5757715b078f78d4486a8d8a
TechTastic <thetechnomancer123@gmail.com>
2023-06-07 16:32:17 -0500

    Changed KettleBlock BlockState

--- common/src/main/java/net/techtastic/tat/block/custom/KettleBlock.java
+++ common/src/main/java/net/techtastic/tat/block/custom/KettleBlock.java
@@ -29,15 +29,15 @@ import net.techtastic.tat.ToilAndTroubleExpectPlatform;
 import net.techtastic.tat.block.TATBlockEntities;
 import net.techtastic.tat.block.entity.DistilleryBlockEntity;
 import net.techtastic.tat.block.entity.KettleBlockEntity;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class KettleBlock extends BaseEntityBlock {
-    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
-    public static final BooleanProperty LIT = BlockStateProperties.LIT;
-    public static final BooleanProperty HANG_ABOVE = BooleanProperty.create("above");
-    public static final BooleanProperty HANG_BLOCK = BooleanProperty.create("block");
-    public static final BooleanProperty HANG_WALL = BooleanProperty.create("wall");
-    public static final BooleanProperty HANG_DOUBLE_WALL = BooleanProperty.create("wall_double");
+    public static final BooleanProperty NORTH = BlockStateProperties.NORTH;
+    public static final BooleanProperty SOUTH = BlockStateProperties.SOUTH;
+    public static final BooleanProperty EAST = BlockStateProperties.EAST;
+    public static final BooleanProperty WEST = BlockStateProperties.WEST;
+    public static final BooleanProperty UP = BlockStateProperties.UP;
 
 
     public KettleBlock(Properties properties) {
@@ -47,126 +47,30 @@ public class KettleBlock extends BaseEntityBlock {
     @Nullable
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext blockPlaceContext) {
-        Direction clickedFace = blockPlaceContext.getClickedFace();
-        Direction lookingDirection = blockPlaceContext.getNearestLookingDirection();
         Level level = blockPlaceContext.getLevel();
         BlockPos pos = blockPlaceContext.getClickedPos();
 
-        BlockState state = this.defaultBlockState()
-                .setValue(FACING, lookingDirection.getOpposite())
-                .setValue(LIT, false)
-                .setValue(HANG_ABOVE, false)
-                .setValue(HANG_BLOCK, false)
-                .setValue(HANG_WALL, false)
-                .setValue(HANG_DOUBLE_WALL, false);
-
-        BlockState east = level.getBlockState(pos.relative(Direction.EAST));
-        BlockState west = level.getBlockState(pos.relative(Direction.WEST));
-        BlockState north = level.getBlockState(pos.relative(Direction.NORTH));
-        BlockState south = level.getBlockState(pos.relative(Direction.SOUTH));
-        BlockState above = level.getBlockState(pos.relative(Direction.UP));
-        BlockState clicked = level.getBlockState(pos.relative(clickedFace.getOpposite()));
-
-        if (east.getBlock() instanceof WallBlock)
-            if (west.getBlock() instanceof WallBlock)
-                return state
-                        .setValue(FACING, blockPlaceContext.getHorizontalDirection().getOpposite())
-                        .setValue(LIT, false)
-                        .setValue(HANG_DOUBLE_WALL, true);
-            else
-                return state
-                        .setValue(FACING, Direction.NORTH)
-                        .setValue(LIT, false)
-                        .setValue(HANG_WALL, true);
-        else if (west.getBlock() instanceof WallBlock)
-            return state
-                    .setValue(FACING, Direction.SOUTH)
-                    .setValue(LIT, false)
-                    .setValue(HANG_WALL, true);
-        else if (north.getBlock() instanceof WallBlock)
-            if (south.getBlock() instanceof WallBlock)
-                return state
-                        .setValue(FACING, blockPlaceContext.getHorizontalDirection().getClockWise())
-                        .setValue(LIT, false)
-                        .setValue(HANG_DOUBLE_WALL, true);
-            else
-                return state
-                        .setValue(FACING, Direction.WEST)
-                        .setValue(LIT, false)
-                        .setValue(HANG_WALL, true);
-        else if (south.getBlock() instanceof WallBlock)
-            return state
-                    .setValue(FACING, Direction.EAST)
-                    .setValue(LIT, false)
-                    .setValue(HANG_WALL, true);
-        else if (!clickedFace.equals(Direction.UP) && clicked.isFaceSturdy(level, pos.relative(clickedFace.getOpposite()), clickedFace, SupportType.CENTER))
-            return state
-                    .setValue(FACING, clickedFace)
-                    .setValue(LIT, false)
-                    .setValue(HANG_BLOCK, true);
-        else if (north.isFaceSturdy(level, pos.relative(Direction.NORTH), Direction.SOUTH, SupportType.CENTER))
-            return state
-                    .setValue(FACING, Direction.NORTH)
-                    .setValue(LIT, false)
-                    .setValue(HANG_BLOCK, true);
-        else if (south.isFaceSturdy(level, pos.relative(Direction.SOUTH), Direction.NORTH, SupportType.CENTER))
-            return state
-                    .setValue(FACING, Direction.SOUTH)
-                    .setValue(LIT, false)
-                    .setValue(HANG_BLOCK, true);
-        else if (east.isFaceSturdy(level, pos.relative(Direction.EAST), Direction.WEST, SupportType.CENTER))
-            return state
-                    .setValue(FACING, Direction.EAST)
-                    .setValue(LIT, false)
-                    .setValue(HANG_BLOCK, true);
-        else if (east.isFaceSturdy(level, pos.relative(Direction.WEST), Direction.EAST, SupportType.CENTER))
-            return state
-                    .setValue(FACING, Direction.WEST)
-                    .setValue(LIT, false)
-                    .setValue(HANG_BLOCK, true);
-        else if (above.isFaceSturdy(level, pos.relative(Direction.UP), Direction.DOWN, SupportType.CENTER))
-            return state
-                    .setValue(FACING, blockPlaceContext.getHorizontalDirection().getOpposite())
-                    .setValue(LIT, false)
-                    .setValue(HANG_ABOVE, true);
-        else
-            return state
-                    .setValue(FACING, blockPlaceContext.getHorizontalDirection().getOpposite())
-                    .setValue(LIT, false);
-    }
-
-    @Override
-    public BlockState rotate(BlockState blockState, Rotation rotation) {
-        Direction rotated = rotation.rotate(blockState.getValue(FACING));
-        return blockState.setValue(FACING, rotated);
-    }
-
-    @Override
-    public BlockState mirror(BlockState blockState, Mirror mirror) {
-        return blockState.rotate(mirror.getRotation(blockState.getValue(FACING)));
+        return getNewState(level, pos);
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(FACING, LIT, HANG_ABOVE, HANG_WALL, HANG_DOUBLE_WALL, HANG_BLOCK);
+        builder.add(NORTH, SOUTH, EAST, WEST, UP);
     }
 
     @Override
     public VoxelShape getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
-        return switch (blockState.getValue(FACING)) {
-            case NORTH, SOUTH -> Block.box(0, 0, 4, 16, 16, 12);
-            default -> Block.box(4, 0, 0, 12, 16, 16);
-        };
+        return ;
     }
 
     @Nullable
     @Override
-    public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
+    public BlockEntity newBlockEntity(@NotNull BlockPos blockPos, @NotNull BlockState blockState) {
         return ToilAndTroubleExpectPlatform.getKettleBlockEntity(blockPos, blockState);
     }
 
     @Override
-    public RenderShape getRenderShape(BlockState blockState) {
+    public @NotNull RenderShape getRenderShape(@NotNull BlockState blockState) {
         return RenderShape.MODEL;
     }
 
@@ -243,19 +147,17 @@ public class KettleBlock extends BaseEntityBlock {
     public void neighborChanged(BlockState blockState, Level level, BlockPos blockPos, Block block, BlockPos blockPos2, boolean bl) {
         super.neighborChanged(blockState, level, blockPos, block, blockPos2, bl);
 
-        BlockState newState = this.getStateForPlacement(BlockPlaceContext.at(
-                new BlockPlaceContext(null, null, null, null),
-                blockPos,
-                blockPos.relative(Direction.NORTH).equals(blockPos2) ? Direction.NORTH :
-                        blockPos.relative(Direction.SOUTH).equals(blockPos2) ? Direction.SOUTH :
-                                blockPos.relative(Direction.EAST).equals(blockPos2) ? Direction.EAST :
-                                        blockPos.relative(Direction.WEST).equals(blockPos2) ? Direction.WEST :
-                                                blockPos.relative(Direction.UP).equals(blockPos2) ? Direction.UP :
-                                                        Direction.DOWN));
+        level.setBlockAndUpdate(blockPos, getNewState(level, blockPos));
+    }
 
-        if (newState == null)
-            newState = this.defaultBlockState();
+    private BlockState getNewState(Level level, BlockPos pos) {
+        BlockState state = this.defaultBlockState();
 
-        level.setBlockAndUpdate(blockPos, newState);
+        return state
+                .setValue(UP, state.isFaceSturdy(level, pos.relative(Direction.UP), Direction.DOWN, SupportType.CENTER))
+                .setValue(NORTH, state.isFaceSturdy(level, pos.relative(Direction.NORTH), Direction.SOUTH, SupportType.FULL))
+                .setValue(SOUTH, state.isFaceSturdy(level, pos.relative(Direction.SOUTH), Direction.NORTH, SupportType.FULL))
+                .setValue(EAST, state.isFaceSturdy(level, pos.relative(Direction.EAST), Direction.WEST, SupportType.FULL))
+                .setValue(WEST, state.isFaceSturdy(level, pos.relative(Direction.WEST), Direction.EAST, SupportType.FULL));
     }
 }
